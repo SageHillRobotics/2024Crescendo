6b9467ab85596eb77530f4f3eddb926bb7a700e5
Kevin Peng <kevinpeng345@gmail.com>
2024-12-27 16:03:36 -0800

    removed deprecated yagsl constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,7 +9,6 @@ import com.pathplanner.lib.util.PIDConstants;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import swervelib.math.Matter;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
@@ -21,10 +20,6 @@ import swervelib.math.Matter;
  */
 public final class Constants
 {
-
-  public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
-  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
-  public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
   public static final class AutonConstants
   {
